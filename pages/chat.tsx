4130e1a230c2ed3450a93f1a477375f0f5b7de3e
bere6255 <clement.bere@yahoo.com>
2022-06-21 13:08:10 +0100

    chat section  done

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -4,8 +4,10 @@ import { css } from '@emotion/css'
 import Text, { headline1 } from '../components/Text'
 import Container from '../components/Container'
 import Layout from '../components/Layout'
+import moment from 'moment'
 import { selecteItem, deleteFromCart, getCart } from '../store/reducers/cart'
 import { useAppDispatch, useAppSelector } from '../hooks/hooks'
+import Icon from '../components/Icon'
 
 // "text-gray-700 font-bold mb-2 lg:mb-4 text-center lg:text-left"
 
@@ -29,7 +31,60 @@ const Carts = () => {
         >
 
           <div className='mx-6 border border-indigo-600'>
-            
+            <div className="my-2 ">
+              <div
+                className={classNames(
+                  'bg-gray-300 text-gray-800 px-2 rounded-lg',
+                  css`
+                background-color: #e9f4ff;
+              `,
+                )}
+              >
+                hi
+              </div>
+              <span className="text-xs mx-2">
+                {moment(new Date).format('MM/DD/YYYY h:mm a')}
+              </span>
+            </div>
+
+            <div className="my-2 text-right ">
+              <div
+                className={classNames(
+                  'bg-gray-300  text-gray-800 text-right px-2 rounded-lg',
+                  css`
+                background-color: #1286ff42;
+              `,
+                )}
+              >
+               hello 
+              </div>
+              <span className="text-xs mx-2">
+                {moment(new Date).format('MM/DD/YYYY h:mm a')}
+              </span>
+            </div>
+
+            <div className="flex justify-between px-2 py-1 w-full border-solid border-2 border-primary-400 rounded-full">
+            <input
+              type="text"
+              value=""
+             
+              className={classNames(
+                'py-1 px-1 mx-1 focus:outline-none  ',
+                css`
+                  width: 90%;
+                `,
+              )}
+              placeholder="Enter Message......"
+              id=""
+            />
+            <button
+             
+              className="bg-blue-500 rounded-lg focus:outline-none hover:bg-blue-400"
+            >
+              {'send '}
+              <Icon name="chevron-right" className="mx-3" size={18} />
+            </button>
+          </div>
           </div>
 
         </Container>
